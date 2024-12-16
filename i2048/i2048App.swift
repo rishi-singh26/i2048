2b5bb41b97d4e3590ca422d8046d373cb65fc71f
rishi-singh26 <rishisingh0831@gmail.com>
2024-12-16 23:08:24 +0530

    Initial Commit

---
+++ i2048/i2048App.swift
@@ -0,0 +1,17 @@
+//
+//  i2048App.swift
+//  i2048
+//
+//  Created by Rishi Singh on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct i2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
