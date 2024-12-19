f23945ef349a8f8167b7a7b68e9125f5bee3b00f
rishi-singh26 <rishisingh0831@gmail.com>
2024-12-19 10:12:46 +0530

    Added the backgroud art model

---
+++ i2048/Models/BackgroundArt.swift
@@ -0,0 +1,42 @@
+//
+//  BackgroundArt.swift
+//  i2048
+//
+//  Created by Rishi Singh on 19/12/24.
+//
+
+
+struct BackgroundArt: Identifiable, Codable {
+    let id: Int
+    let name: String
+    let dribbleLink: String
+    let profileImage: String
+    let images: [ImageItem]
+}
+
+struct ImageItem: Identifiable, Codable {
+    let id: Int
+    let url: String
+    let previewUrl: String
+    let dribbleUrl: String
+    let name: String
+    let mode: Bool
+    let forGroundColor: String
+    let gameVictoryColor: String
+    let gameLossColor: String
+    let backGroundColor: String
+    let color2: String
+    let color4: String
+    let color8: String
+    let color16: String
+    let color32: String
+    let color64: String
+    let color128: String
+    let color256: String
+    let color512: String
+    let color1024: String
+    let color2048: String
+    let color4096: String
+    let color8192: String
+    let color16384: String
+}
